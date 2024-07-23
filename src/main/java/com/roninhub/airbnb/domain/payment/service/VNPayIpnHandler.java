d4ffe59e119114f82cec4d226b84cdabb8e64b8c
vking34 <vuong.lv34@gmail.com>
2024-07-23 09:58:20 +0700

    refactor(payment): clean

---
+++ src/main/java/com/roninhub/airbnb/domain/payment/service/VNPayIpnHandler.java
@@ -0,0 +1,49 @@
+package com.roninhub.airbnb.domain.payment.service;
+
+
+import com.roninhub.airbnb.domain.booking.service.BookingService;
+import com.roninhub.airbnb.domain.common.exception.BusinessException;
+import com.roninhub.airbnb.domain.payment.constant.VNPayParams;
+import com.roninhub.airbnb.domain.payment.constant.VnpIpnResponseConst;
+import com.roninhub.airbnb.domain.payment.dto.response.IpnResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class VNPayIpnHandler implements IpnHandler {
+
+    private final VNPayService vnPayService;
+
+    private final BookingService bookingService;
+
+
+    public IpnResponse process(Map<String, String> params) {
+        if (!vnPayService.verifyIpn(params)) {
+            return VnpIpnResponseConst.SIGNATURE_FAILED;
+        }
+
+        IpnResponse response;
+        try {
+            var bookingId = Long.parseLong(params.get(VNPayParams.TXN_REF));
+            bookingService.markBooked(bookingId);
+            response = VnpIpnResponseConst.SUCCESS;
+        }
+        catch (BusinessException e) {
+            switch (e.getResponseCode()) {
+                case BOOKING_NOT_FOUND -> response = VnpIpnResponseConst.ORDER_NOT_FOUND;
+                default -> response = VnpIpnResponseConst.UNKNOWN_ERROR;
+            }
+        }
+        catch (Exception e) {
+            response = VnpIpnResponseConst.UNKNOWN_ERROR;
+        }
+
+        log.info("VNPay Ipn Response: {}", response);
+        return response;
+    }
+}

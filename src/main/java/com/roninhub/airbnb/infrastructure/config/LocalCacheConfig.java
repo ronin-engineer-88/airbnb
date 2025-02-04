f5e42c5016615c9d85b61fa910976413f15f5a0f
vking34 <vuong.lv34@gmail.com>
2025-02-04 11:45:37 +0700

    fix(memory-leak): add ttl + max size for local cache

--- src/main/java/com/roninhub/airbnb/infrastructure/config/LocalCacheConfig.java
+++ src/main/java/com/roninhub/airbnb/infrastructure/config/LocalCacheConfig.java
@@ -5,12 +5,17 @@ import com.google.common.cache.CacheBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 public class LocalCacheConfig {
 
     @Bean
     public Cache<String, String> localCache() {
         // Misconfiguration: without specifying any maximum size or expiration
-        return CacheBuilder.newBuilder().build();
+        return CacheBuilder.newBuilder()
+                .maximumSize(10000L)
+                .expireAfterWrite(3, TimeUnit.SECONDS)  // Just for reference, not for production
+                .build();
     }
 }
